d9e91eb15e8d1dacde098eaf637ad9b2beafe6f4
Djimovanberlo <djimovanberlo@gmail.com>
2024-12-18 14:27:21 +0100

    init setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { GameBoardComponent } from './game-board/game-board.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBarComponent, GameBoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'stackin-paper';
---
+++ src/app/game-board/game-board.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'game-board',
+  templateUrl: './game-board.component.html',
+  styleUrl: './game-board.component.scss',
+})
+export class GameBoardComponent {}
