5a92bc977df1dae813623ef7ea8385a61b273990
Djimovanberlo <djimovanberlo@gmail.com>
2025-01-10 13:04:48 +0100

    pass style from prop to CSS property

--- src/app/card-target/card-target.component.ts
+++ src/app/card-target/card-target.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
+import { NgStyle } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { CardTarget } from '../card';
 
 @Component({
   selector: 'app-card-target',
-  imports: [CardComponent],
+  imports: [CardComponent, NgStyle],
   templateUrl: './card-target.component.html',
   styleUrl: './card-target.component.scss',
 })
