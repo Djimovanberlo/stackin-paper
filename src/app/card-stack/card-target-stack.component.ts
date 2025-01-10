691c09cad2db70dd67cf8285e32ece047beca10b
Djimovanberlo <djimovanberlo@gmail.com>
2025-01-10 13:15:42 +0100

    basic card stack styles

--- src/app/card-stack/card-target-stack.component.ts
+++ src/app/card-stack/card-target-stack.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { CardTargetStack } from '../card';
 import { GameColour } from '../game-colour';
 import { CardTargetComponent } from '../card-target/card-target.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-card-target-stack',
-  imports: [CardTargetComponent],
+  imports: [CardTargetComponent, NgStyle],
   templateUrl: './card-target-stack.component.html',
   styleUrl: './card-target-stack.component.scss',
 })
